c8bb04685e9e7d558761869a23c422dad82ec344
Crowdbotics <engineering@crowdbotics.com>
2022-12-05 15:02:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/scarappcode4_dev_48870/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scarappcode4_dev_48870;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "scarappcode4_dev_48870";
+  }
+}
